7c7643d8fa7546f3975441327bd25618cf50df33
Mahbub <mahbuburemon60@gmail.com>
2024-03-12 14:42:20 +0600

    barcode & qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// // App.js
+// import React from 'react';
+// import Barcode from 'react-barcode';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Generate Barcode in React.js</h1>
+
+//       <div>
+//         <h2>Code 128 Barcode Example</h2>
+//         <Barcode value="123456789" />
+//       </div>
+//       <div>
+//         <h2>Code 39 Barcode Example</h2>
+       
+//         <Barcode
+//             value="123456789"
+//             width={1}
+//             height={40}
+//             fontSize={12}
+//             margin={10}
+//             background="#f5f5f5"
+//             lineColor="#000000"
+//         />
+
+//       </div>
+//       <div className="container">
+//       <Barcode value="123456789" format="CODE39" />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import QRCode from 'qrcode.react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate QR Code in React.js</h1>
+      <QRCode
+        value="123456"
+        size={200}
+        level={"H"}
+        bgColor={"#ffffff"}
+        fgColor={"#000000"}
+      />
+
     </div>
   );
 }
 
 export default App;
+
+
